c6b8da3a274358126531653d7934d6f8f5780642
Marlon Perez <perezmarlon538@gmail.com>
2024-04-22 12:18:17 -0600

    style(prettierrc): Se agrega .prettierrc y se da aplica formato a ficheros existentes

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,8 +1,8 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
+import type { LoaderFunctionArgs } from '@remix-run/node';
+import { authenticate } from '../shopify.server';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
+    await authenticate.admin(request);
 
-  return null;
+    return null;
 };
--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -1,44 +1,44 @@
-import "@shopify/shopify-app-remix/adapters/node";
+import '@shopify/shopify-app-remix/adapters/node';
 import {
-  ApiVersion,
-  AppDistribution,
-  DeliveryMethod,
-  shopifyApp,
-} from "@shopify/shopify-app-remix/server";
-import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
-import { restResources } from "@shopify/shopify-api/rest/admin/2024-04";
-import prisma from "./db.server";
+    ApiVersion,
+    AppDistribution,
+    DeliveryMethod,
+    shopifyApp,
+} from '@shopify/shopify-app-remix/server';
+import { PrismaSessionStorage } from '@shopify/shopify-app-session-storage-prisma';
+import { restResources } from '@shopify/shopify-api/rest/admin/2024-04';
+import prisma from './db.server';
 
 const shopify = shopifyApp({
-  apiKey: process.env.SHOPIFY_API_KEY,
-  apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  apiVersion: ApiVersion.April24,
-  scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.SHOPIFY_APP_URL || "",
-  authPathPrefix: "/auth",
-  sessionStorage: new PrismaSessionStorage(prisma),
-  distribution: AppDistribution.AppStore,
-  restResources,
-  webhooks: {
-    APP_UNINSTALLED: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/webhooks",
+    apiKey: process.env.SHOPIFY_API_KEY,
+    apiSecretKey: process.env.SHOPIFY_API_SECRET || '',
+    apiVersion: ApiVersion.April24,
+    scopes: process.env.SCOPES?.split(','),
+    appUrl: process.env.SHOPIFY_APP_URL || '',
+    authPathPrefix: '/auth',
+    sessionStorage: new PrismaSessionStorage(prisma),
+    distribution: AppDistribution.AppStore,
+    restResources,
+    webhooks: {
+        APP_UNINSTALLED: {
+            deliveryMethod: DeliveryMethod.Http,
+            callbackUrl: '/webhooks',
+        },
     },
-  },
-  hooks: {
-    afterAuth: async ({ session }) => {
-      shopify.registerWebhooks({ session });
+    hooks: {
+        afterAuth: async ({ session }) => {
+            shopify.registerWebhooks({ session });
+        },
     },
-  },
-  future: {
-    v3_webhookAdminContext: true,
-    v3_authenticatePublic: true,
-    v3_lineItemBilling: true,
-    unstable_newEmbeddedAuthStrategy: true,
-  },
-  ...(process.env.SHOP_CUSTOM_DOMAIN
-    ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
-    : {}),
+    future: {
+        v3_webhookAdminContext: true,
+        v3_authenticatePublic: true,
+        v3_lineItemBilling: true,
+        unstable_newEmbeddedAuthStrategy: true,
+    },
+    ...(process.env.SHOP_CUSTOM_DOMAIN
+        ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
+        : {}),
 });
 
 export default shopify;
